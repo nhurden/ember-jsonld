857c8b16bd56ef3e85e6734b2c27c0c46551d26b
Nicholas Hurden <nicholas@nhurden.com>
2014-09-05 15:51:53 +1000

    Add ember-addon keyword and ember-addon-main.

--- dist/named-amd/main.js
+++ dist/named-amd/main.js
@@ -1,3 +1,52 @@
+define("ember-jsonld/ember-cli-jsonld",
+  [],
+  function() {
+    "use strict";
+    'use strict';
+
+    var path = require('path');
+    var fs   = require('fs');
+
+    function EmberCLIJsonLd(project) {
+      this.project = project;
+      this.name    = 'Ember CLI jsonld';
+    }
+
+    function unwatchedTree(dir) {
+      return {
+        read:    function() { return dir; },
+        cleanup: function() { }
+      };
+    }
+
+    EmberCLIJsonLd.prototype.treeFor = function treeFor(name) {
+      var treePath =  path.join('node_modules', 'ember-jsonld', name);
+
+      if (fs.existsSync(treePath)) {
+        return unwatchedTree(treePath);
+      }
+    };
+
+    EmberCLIJsonLd.prototype.included = function included(app) {
+      this.app = app;
+
+      this.app.import('vendor/ember-jsonld/dist/named-amd/main.js', {
+        exports: {
+          'ember-jsonld': [
+            'expand',
+            'compact',
+            'flatten',
+            'frame',
+            'fromRDF',
+            'toRDF',
+            'normalize'
+          ]
+        }
+      });
+    };
+
+    module.exports = EmberCLIJsonLd;
+  });
 define("ember-jsonld",
   ["ember","jsonld","exports"],
   function(__dependency1__, __dependency2__, __exports__) {
