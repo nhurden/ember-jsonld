1438405fa9eb10aa019826677a8b8d7baf9b3153
Nicholas Hurden <git@nhurden.com>
2016-03-20 20:46:49 +1100

    Run the addon generator.

--- Brocfile.js
+++
@@ -1,9 +0,0 @@
-var distes6 = require('broccoli-dist-es6-module');
-module.exports = distes6('lib', {
-  global: 'ld',
-  packageName: 'ember-jsonld',
-  main: 'main',
-  shim: {
-    'ember': 'Ember'
-  }
-});
